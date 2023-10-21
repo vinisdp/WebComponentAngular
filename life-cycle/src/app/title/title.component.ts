69be72337d18242c9b18c188da78c2ef82ff83a4
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-10-21 16:33:22 -0400

    Factore: Utilizando OnChanges

--- life-cycle/src/app/title/title.component.ts
+++ life-cycle/src/app/title/title.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
 
-  nome: String = 'Vinicius';
+  @Input() nome: String = '';
 
-  constructor() { }
+  constructor() {
+    console.log(`Contrutor ${this.nome}`)
+  }
+
+  ngOnChanges(): void {
+    console.log(`OnChanges ${this.nome}`);
+  }
 
   ngOnInit(): void {
-    this.nome = `Olá ${this.nome}`;
+    console.log(`OnInit ${this.nome}`);
   }
 
 }
